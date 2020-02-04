2e235064c5f2fa30695ffbaec4f2deabe404dc5e
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-02-04 17:34:23 -0500

    added Javascript demo

---
+++ self_portrait/sketch.js
@@ -0,0 +1,13 @@
+function setup () { 
+	createCanvas (640,360);
+
+}
+
+function draw () {
+	background (220);
+	ellipse(320,180 , 100);
+	ellipse(380,180,60,60);
+	ellipse(310,160,20);
+	ellipse(280,160,20);
+
+}
