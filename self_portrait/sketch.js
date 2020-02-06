ae02fe426b1305b70853dc131d4ce274d63602f1
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-02-06 17:02:45 -0500

    Added colors and whatnot.
    scription..

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -4,10 +4,26 @@ function setup () {
 }
 
 function draw () {
-	background (220);
-	ellipse(320,180 , 100);
-	ellipse(380,180,60,60);
+	// SHAPES WITH X,Y coordinates and size
+	background (200,300,88);
+		ellipse(380,180,60,60); //ponytail
+	ellipse(320,180 , 100);// face 
+	//rectMode(CENTER)
+
+	noFill();
+	stroke('red');
+	strokeWeight("5");
 	ellipse(310,160,20);
+
+	//fill('blue')
 	ellipse(280,160,20);
 
+	rect(280,180, 40,20,10);
+	arc(360,180,20,40,HALF_PI, HALF_PI);
+
+triangle(150, 70, 180, 100, 220, 60);
+
 }
+
+// shapes that are only x,y points 
+ 
