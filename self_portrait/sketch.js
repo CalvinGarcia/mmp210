a76d63d7341d05f15e65cb2541cd955dc91235f0
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-02-11 15:57:11 -0500

    Sketch updates
    Changed sketch...

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -5,25 +5,52 @@ function setup () {
 
 function draw () {
 	// SHAPES WITH X,Y coordinates and size
-	background (200,300,88);
-		ellipse(380,180,60,60); //ponytail
-	ellipse(320,180 , 100);// face 
-	//rectMode(CENTER)
+	background ('white');
+	//ellipse(260,120,130);//head
+	//arc(226,80,30,22,)
+	//ellipse(226,80,30);//left eye
+	//ellipse(236,100,30);//nose
+	//ellipse()
 
-	noFill();
-	stroke('red');
-	strokeWeight("5");
-	ellipse(310,160,20);
+	//curve(73,24,73,61,15,65,15,65);
+	
+	fill('blue');
 
-	//fill('blue')
-	ellipse(280,160,20);
+	ellipse(300,200,120,170,20);
 
-	rect(280,180, 40,20,10);
-	arc(360,180,20,40,HALF_PI, HALF_PI);
 
-triangle(150, 70, 180, 100, 220, 60);
+	noStroke()
+	fill('blue')
+	ellipse(340,160,50,50,)//hair1
+	ellipse(350,170,50,50)
+	ellipse(354,180,50,50)
+	ellipse(360,160,40,40)
 
+	//ellipse(280,196,4,4)
+
+	//head and eyes
+	fill('white');
+	stroke('black')
+	ellipse(260,160,20,20,)//right eye
+	ellipse(310,160,20,20,)//left eye
+	ellipse(280,190,40,20,)//nose
+	ellipse(280,220,10,10,)//mouth
+
+	noFill()
+	curve(300, 120, 300 ,110, 300, 80, 415, 65, 400, 215, 165, 156)
+	curve(300, 120, 296 ,110, 300, 80, 415, 65, 400, 215, 165, 156)
+	line(248,150,270,150)//eyebrow
+	line(298,150,330,150)//eyebrow
+
+	line(245,240,242,250) //beard
+	line(249,240,247,255) //beard
+	line(251,245,249,259) //beard
+	//arc(300,200,120,10,0,PI,);
+
+	ellipse(266,160,8,8)//right eye
+	ellipse(316,160,8,8)
 }
+
 
 // shapes that are only x,y points 
  
