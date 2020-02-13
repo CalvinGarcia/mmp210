1fef854000db10943cfd00cf0db189fb870f4b3f
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-02-13 12:57:22 -0500

    updated variables
    updated in variables.

--- variables/sketch.js
+++ variables/sketch.js
@@ -1,5 +1,5 @@
-x =200
-
+var x = 200
+var y = 100
 
 
 function setup () { 
@@ -24,43 +24,43 @@ function draw () {
 
 	fill('blue');
 
-	ellipse(x + 100,200,120,170,20);// head
+	ellipse(x + 100, x ,y + 20,y + 70,20);// head
 
-
+	
 	noStroke()
 	fill('blue')
-	ellipse(340,160,50,50,);//hair1
-	ellipse(350,170,50,50);
-	ellipse(354,180,50,50);
-	ellipse(360,160,40,40);
+	ellipse( x + 140, y + 60,y/2,y/2,);//hair1
+	ellipse( x + 150, y + 70, y/2,y/2);
+	ellipse( x + 154, y + 80,y/2,y/2);
+	ellipse( x + 160, y + 60, 40, 40);
 
 	//ellipse(280,196,4,4)
 
 	//head and eyes
 	fill('white');
 	stroke('black');
-	ellipse(260,160,20,20,);//right eye
-	ellipse(310,160,20,20,);//left eye
+	ellipse(x + 60, y + 60,y/5,y/5,);//right eye
+	ellipse(x + 110,y + 60,20,20,);//left eye
 	fill('black')
 	//arc(280,210,120,90,180,360,HALF_PI,OPEN)
 	fill('white')
-	ellipse(280,190,40,20,);//nose
-	ellipse(280,220,10,10,);//mouth
+	ellipse(x + 80,y + 90,y-60,y-80);//nose
+	ellipse(x + 80,x + 20,10,10,);//mouth
 
 	noFill();
-	curve(300, 120, 300 ,110, 300, 80, 415, 65, 400, 215, 165, 156);
-	curve(300, 120, 296 ,110, 300, 80, 415, 65, 400, 215, 165, 156);
+	curve(x + y, y + 20, x + y ,y + 10, x + y, 80, x + x + 15,y-35 , x + x, x + 15, y + 65, y + 56);
+	curve(x + y, y + 20, x + 96 ,y + 10, x + y, y - 20, (x * 2) + 15, 65, x * 2, x + 15, y + 65, y + 56);
 	fill('black')
-	rect(248,150,24,5);//eyebrow
-	rect(298,150,24,5);//eyebrow
+	rect(x + 48,y + 50,24,5);//eyebrow
+	rect(x + 98,y + 50,24,5);//eyebrow
 
-	line(245,240,242,250); //beard
-	line(249,240,247,255);//beard
-	line(251,245,249,259); //beard
+	line(x + 45,x + 40,x + 42,x + 50); //beard
+	line(x + 49,x + 40,x + 47,x + 55);//beard
+	line(x + 51,x + 45,x + 49,x + 59); //beard
 	//arc(300,200,120,10,0,PI,);
 
-	ellipse(266,160,8,8);//right eye
-	ellipse(316,160,8,8);
+	ellipse(x + 66,y + 60,8,8);//right eye
+	ellipse(x + y + 16,y + 60,8,8);
 }
 
 
