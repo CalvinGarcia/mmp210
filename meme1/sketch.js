6008ed6560dbf27d45ee82961a998910551122bb
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-02-27 17:18:39 -0500

    added image to meme
    meme tings.

--- meme1/sketch.js
+++ meme1/sketch.js
@@ -4,43 +4,65 @@ Meme 1
 
 */
 
+var img;
+
+function preload(){
+
+	img = loadImage("hide.jpg");
+}
+
 function setup(){
-	createCanvas(400,400);
+	createCanvas(600,600);
 
 }
 function draw(){
 
-	background('#f5902c');
 
-	textFont('Futura'||'impact')
-	textSize(55);
-	textStyle(BOLD);
-	//stroke('green');
-	strokeWeight(10);
-	text("cannon ball!",width/2,height/3.5);
-	textAlign(CENTER);
-	
 	var r = mouseX
 	var g = mouseY
-	var b = 0 
+	var b = 0 	
 
-	fill(r,g,b);
+	background(r,g,b);
+
+	
+
+	image(img, 0, 100,600,325);
+
+	var str= "Who's up for a fortnite?"
+	var length = str.length; 
+	var n = map(mouseX,0,width,0,length);
+	var displayString = str.substring(0,n);
+	text(displayString,width/2,480);
+
+	fill('white');
+	stroke('black');
+	strokeWeight(4);
+	textFont('impact');
+	textSize(40);
+	textStyle(BOLD);
+	//text("Who's up for a fortnite",width/2,480);
+	textAlign(CENTER);
+	
+
+
+
 
 
 
 	var x= 50;
 	var y= 50;
 	var d=50;
+
 	
-	var face= (x,y,d);
+	//var face= (x,y,d);
 
-	var Sam= 'I do not like them, Sam I am'
+	var Sam= 'Me trying to relate to my classmates'
 
-	textSize(20);
+	textSize(36);
 	strokeWeight(2);
-	text(Sam, width/3.5,height/2,100,100);
+	text(Sam,width/2,80);
 
-	ellipse(width/2,height/2,face)
+	//ellipse(width/2,height/2,face)
 
 
 }
