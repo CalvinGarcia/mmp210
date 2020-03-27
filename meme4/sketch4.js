e0824c78ebf0ae9eece77935de16d29e269a05ce
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-03-27 18:02:19 -0400

    meme4 complete
    meme4 complete

--- meme4/sketch4.js
+++ meme4/sketch4.js
@@ -7,33 +7,71 @@ Meme 4
 var calvy  
 var r
 var s
-
+var speed
+var loading = 'Meme Loading';
+var stay = 'Stay Tuned';
+var dot = '...'
+var x = frameCount * PI / 100;
 
  function setup(){ 
  	createCanvas (600,600);
  	calvy = loadImage("SmokingCalvy.jpg");
 
- 	r = rotate(frameCount * 80, TWO_PI);
- 	s = scale(frameCount/2 );
- 	
+ 	r = 0;
+ 	s = 2;
+ 	speed = .05;
 
  }
 
  function draw() { 
  	background('white');
 
-//goal is to zoom in regular and then spin backward 
+
+	push();
+
+		
+	textFont('impact');
+ 	textAlign(CENTER);
+ 	textSize(60);
+ 	color('black');		
+	text(stay,width/2, height-540);
+ 	
+ 	var r = frameCount * PI / 200;
+
+ 	
+	shearX(r);
+
+ 	text(loading, width/2, height-20);
+ 	
+ 	text(dot, 500,height-20);
+ 	
+	//var h = dot.substring();
+	//var displayString = dot.substring(0,3);
+	
+pop();
 
  	translate( width/2, height/2);
- 	rotate(frameCount * 5, TWO_PI);
- 	scale(frameCount/20 );
  	
+ 
+
+ 	rotate(r);
+ 		r+= .01 * PI; 
+ 	scale(s);
+ 		s+= speed;	
  	
 
+
  	imageMode(CENTER);
- 	image(calvy, 0 , 0, 50, 50);	
- 		if (frameCount > 1000) {
- 			scale(framecount %;
+ 	image(calvy, 0 , 0, 80, 80);
+
+ 	
+
+ 		if ( s > 4 || s < 2) { 
+ 			speed *= -1; 
+
+
+
+ 
 
  		}
 
